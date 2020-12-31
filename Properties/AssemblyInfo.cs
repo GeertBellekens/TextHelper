235ea067ed5a7b138f83303205f039538215f17c
Geert Bellekens <geert@bellekens.com>
2020-12-31 11:09:24 +0100

    update targetFramework

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("377dce16-fbb7-4611-8b03-40bdf76ffe9b")]
